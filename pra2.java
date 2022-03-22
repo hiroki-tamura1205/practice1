c47b2ee914abf82753880e01b41d2061ae871026
Hiroki Tamura <hiroki.tamura.sys21@morijyobi.ac.jp>
2022-03-22 14:42:05 +0900

    ab
    cd

--- pra2.java
+++ pra2.java
@@ -2,7 +2,7 @@ public class pra2 {
 	public static void main( String[] args ) {
         int cnt = 0;
         while( i < 100 ) {
-            System.out.println"Hello World" );
+            System.out.println( "Hello World" );
             cnt++;
         }
     }
